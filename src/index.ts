2cca4122494b6ccb0acaa581d6645c81198de326
Endel Dreyer <endel.dreyer@gmail.com>
2019-02-13 16:50:46 -0200

    fix measuring bytes sent.

--- src/index.ts
+++ src/index.ts
@@ -271,10 +271,10 @@ for (let i = 0; i < numClients; i++) {
         });
 
         // overwrite original send function to trap sent bytes.
-        const _send = room.connection.send;
-        room.connection.send = function(data) {
-            bytesSent += data.length;
-            _send.call(room.connection, data);
+        const _send = room.connection.ws.send;
+        room.connection.ws.send = function(data) {
+            bytesSent += data.byteLength;
+            _send.call(room.connection.ws, data);
         }
 
         clientsConnected++;
